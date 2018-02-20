3b7bfeb157f83902a7bce989f133b8054ef6ed1f
erikrdgz <erikrdgz@hotmail.com>
2018-02-19 20:21:41 -0700

    Finished adding pages

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,24 +1,33 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "Erik Rodriguez"
+  end
+
   test "should get home" do
     get pages_home_url
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get projects" do
     get pages_projects_url
     assert_response :success
+    assert_select "title", "Projects | #{@base_title}"
   end
 
   test "should get about" do
     get pages_about_url
     assert_response :success
+    assert_select "title", "About | #{@base_title}"
   end
 
   test "should get contact" do
     get pages_contact_url
     assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
   end
 
 end
