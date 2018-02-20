4e2a7289b6b36a1fde6d2fe93e4004548092e87f
erikrdgz <erikrdgz@hotmail.com>
2018-02-19 21:27:57 -0700

    Began adding styling

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,11 @@ Rails.application.routes.draw do
 
   get 'pages/home'
 
-  get 'pages/projects'
+  get '/projects', to: 'pages#projects'
 
-  get 'pages/about'
+  get '/about', to: 'pages#about'
 
-  get 'pages/contact'
+  get '/contact', to: 'pages#contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
 end
