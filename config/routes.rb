2879be71fa64217ff41a452e398223e0ae55c6b3
erikrdgz <erikrdgz@hotmail.com>
2018-02-19 20:04:39 -0700

    Add Pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
+  get 'pages/projects'
+
+  get 'pages/about'
+
+  get 'pages/contact'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#hello'
 end
